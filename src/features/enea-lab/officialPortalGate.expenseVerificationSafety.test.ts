cff6fa8b3a8eff80d6f9214013a7a5857f445f33
ulbereta <Ulbereta@gmail.com>
2026-08-14 14:11:10 -0400

    Allinea la fixture spesa al sesso ENEA valido

--- src/features/enea-lab/officialPortalGate.expenseVerificationSafety.test.ts
+++ src/features/enea-lab/officialPortalGate.expenseVerificationSafety.test.ts
@@ -19,7 +19,8 @@ function readyMappedWithExpenseSource(
       fields: section.fields.map((field) => {
         if (!field.required) return field;
         let value = field.value;
-        if (field.id === "intervento.data_inizio") value = "01/01/2026";
+        if (field.id === "beneficiario.sesso") value = "M";
+        else if (field.id === "intervento.data_inizio") value = "01/01/2026";
         else if (field.id === "intervento.data_fine") value = "02/01/2026";
         else if (field.id === "impianto.numero_generatori") value = "1";
         else if (field.id === "impianto.rendimento") value = "95";
